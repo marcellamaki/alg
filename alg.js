726b95181b60261f94a245ed6c1191d103a73a07
Marcella Maki <marcellamaki@gmail.com>
2018-01-07 22:57:50 -0500

    print string

--- alg.js
+++ alg.js
@@ -17,3 +17,14 @@
     return true
   }
 }
+
+function printString(myString) {
+  console.log(myString[0]);
+ 
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}
